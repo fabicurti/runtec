0b3c35ca24db828aa52e90834aa7cca2ca1453f2
Fabiane Curti Felix <fcurti@gmail.com>
2025-08-15 12:50:50 -0300

    yaml26

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,13 +5,6 @@ set -e
 chown -R www-data:www-data /var/www/html
 chmod -R 775 storage bootstrap/cache public/build || true
 
-# Gera assets se não existirem
-if [ ! -f "public/build/manifest.json" ]; then
-  echo "Manifest.json não encontrado. Rodando npm install + build..."
-  npm ci
-  npm run build
-fi
-
 # Limpa caches Laravel
 php artisan config:clear || true
 php artisan cache:clear || true
