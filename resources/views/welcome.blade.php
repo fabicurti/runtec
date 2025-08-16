b6c32faf359a5f8002337edb49b559d34591bcb0
Fabiane Curti Felix <fcurti@gmail.com>
2025-08-16 11:13:25 -0300

    yaml59

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,79 +1,59 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body class="bg-gray-100 flex items-center justify-center h-screen">
 
 @auth
     <script>window.location.href = "{{ route('dashboard') }}";</script>
 @else
     <x-guest-layout>
-    <div class="bg-white p-8  w-full max-w-md">
-        <h1 class="text-2xl font-bold mb-4 text-center">Acesse sua conta</h1>
+        <div class="bg-white p-8  w-full max-w-md">
+            <h1 class="text-2xl font-bold mb-4 text-center">Acesse sua conta</h1>
 
-        <!-- Formulário de login -->
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+            <!-- Formulário de login -->
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-            <!-- Email Address -->
-            <div>
-                <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
-            </div>
+                <!-- Email Address -->
+                <div>
+                    <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Senha')" />
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-input-label for="password" :value="__('Senha')" />
 
-                <x-text-input id="password" class="block mt-1 w-full"
-                              type="password"
-                              name="password"
-                              required autocomplete="current-password" />
+                    <x-text-input id="password" class="block mt-1 w-full"
+                                  type="password"
+                                  name="password"
+                                  required autocomplete="current-password" />
 
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Lembre-se') }}</span>
-                </label>
-            </div>
+                <!-- Remember Me -->
+                <div class="block mt-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                        <span class="ms-2 text-sm text-gray-600">{{ __('Lembre-se') }}</span>
+                    </label>
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Esqueceu sua senha?') }}
-                    </a>
-                @endif
+                <div class="flex items-center justify-end mt-4">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                            {{ __('Esqueceu sua senha?') }}
+                        </a>
+                    @endif
 
-                <x-primary-button class="ms-3">
-                    {{ __('Entrar') }}
-                </x-primary-button>
-            </div>
-        </form>
+                    <x-primary-button class="ms-3">
+                        {{ __('Entrar') }}
+                    </x-primary-button>
+                </div>
+            </form>
 
-        <p class="mt-4 text-center text-sm">
-            Não é cadastrado ainda?
-            <a href="{{ route('register') }}" class="text-blue-600 hover:underline">Cadastre-se</a>
-        </p>
-    </div>
+            <p class="mt-4 text-center text-sm">
+                Não é cadastrado ainda?
+                <a href="{{ route('register') }}" class="text-blue-600 hover:underline">Cadastre-se</a>
+            </p>
+        </div>
     </x-guest-layout>
 @endauth
-</body>
-</html>
