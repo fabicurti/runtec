023f8cbaefa0937c3a9bac236a43e4a21fe70879
Fabiane Curti Felix <fcurti@gmail.com>
2025-08-16 11:03:43 -0300

    yaml58

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,21 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100 flex items-center justify-center h-screen">
 
 @auth
     <script>window.location.href = "{{ route('dashboard') }}";</script>
@@ -58,4 +75,5 @@
     </div>
     </x-guest-layout>
 @endauth
-</x-app-layout>
+</body>
+</html>
